22aebb87439a944e11234e1dac7a862818e35454
7alias <xc.navi@gmail.com>
2022-02-05 18:07:16 +0200

    King doesn't move

--- src/King.java
+++ src/King.java
@@ -1,12 +1,32 @@
 public class King extends ChessPiece {
+
     public King(String color) {
         super(color);
     }
 
-
     @Override
     public String getColor() {
-        return this.color;
+        return color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (checkPos(line) && checkPos(column) && checkPos(toLine) && checkPos(toColumn)) {
+            if (Math.abs(line - toLine) > 1 || Math.abs(column - toColumn) > 1) return false;
+
+            if (isUnderAttack(chessBoard, toLine, toColumn)) return false;
+
+            if (chessBoard.board[toLine][toColumn] != null) {
+                return !chessBoard.board[toLine][toColumn].getColor().equals(color);
+            }
+
+            return true;
+        } else return false;
+    }
+
+    @Override
+    public String getSymbol() {
+        return "K";
     }
 
     public boolean isUnderAttack(ChessBoard chessBoard, int line, int column) {
@@ -20,25 +40,8 @@ public class King extends ChessPiece {
                     }
                 }
             }
-            return false;
-        } else return false;
-    }
-
-    @Override
-    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        if (checkPos(line) && checkPos(column) && checkPos(toLine) && checkPos(toColumn)) {
-            if (Math.abs(line - toLine) > 1 || Math.abs(column - toColumn) > 1) return false;
-            if (isUnderAttack(chessBoard, toLine, toColumn)) return false;
-            if (chessBoard.board[toLine][toColumn] != null) {
-                return !chessBoard.board[toLine][toColumn].getColor().equals(color);
-            }
             return true;
         } else return false;
-    }
-
-    @Override
-    public String getSymbol() {
-        return "K";
     }
 
     public boolean checkPos(int pos) {
