0efae1131f6ed85a22301c58346883053dee04f3
Ilya Maximov <xc.navi@gmail.com>
2022-03-17 12:07:24 +0200

    Code checked, all figures can move, Pawn needs correction: 2 steps everytime instead of second 1 move.

--- src/Rook.java
+++ src/Rook.java
@@ -10,11 +10,15 @@ public class Rook extends ChessPiece {
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        if (checkPos(line) && checkPos(column) && checkPos(toLine) && checkPos(toColumn)) {
+        if (checkPos(line)
+                && checkPos(column)
+                && checkPos(toLine)
+                && checkPos(toColumn)) {
             if (column == toColumn) {
                 for (int i = getMin(line, toLine); i < getMax(line, toLine); i++) {
                     if (chessBoard.board[i][column] != null) {
-                        if (chessBoard.board[i][column] == this && i == getMax(line, toLine)) return false;
+                        if (chessBoard.board[i][column] == this
+                                && i == getMax(line, toLine)) return false;
                         else if (chessBoard.board[i][column].getColor().equals(this.color) && i == toLine)
                             return false;
                         else if (!chessBoard.board[i][column].getColor().equals(this.color) && i == toLine)
@@ -22,27 +26,34 @@ public class Rook extends ChessPiece {
                     }
                 }
                 if (chessBoard.board[toLine][column] != null) {
-                    if (chessBoard.board[toLine][toColumn].getColor().equals(this.color) && chessBoard.board[toLine][column] != this)
+                    if (chessBoard.board[toLine][toColumn].getColor().equals(this.color)
+                            && chessBoard.board[toLine][column] != this)
                         return false;
                     else
-                        return !chessBoard.board[toLine][column].getColor().equals(this.color) && chessBoard.board[toLine][column] != this;
+                        return !chessBoard.board[toLine][column].getColor().equals(this.color)
+                                && chessBoard.board[toLine][column] != this;
                 } else return true;
 
             } else if (line == toLine) {
                 for (int i = getMin(toColumn, column); i < getMax(column, toColumn); i++) {
                     if (chessBoard.board[line][i] != null) {
-                        if (chessBoard.board[line][i] == this && i == getMax(column, toColumn)) return false;
-                        else if (chessBoard.board[line][i].getColor().equals(this.color) && i == toColumn)
+                        if (chessBoard.board[line][i] == this && i
+                                == getMax(column, toColumn)) return false;
+                        else if (chessBoard.board[line][i].getColor().equals(this.color)
+                                && i == toColumn)
                             return false;
-                        else if (!chessBoard.board[line][i].getColor().equals(this.color) && i == toColumn)
+                        else if (!chessBoard.board[line][i].getColor().equals(this.color)
+                                && i == toColumn)
                             return true;
                     }
                 }
                 if (chessBoard.board[toLine][toColumn] != null) {
-                    if (chessBoard.board[toLine][toColumn].getColor().equals(this.color) && chessBoard.board[toLine][toColumn] != this)
+                    if (chessBoard.board[toLine][toColumn].getColor().equals(this.color)
+                            && chessBoard.board[toLine][toColumn] != this)
                         return false;
                     else
-                        return !chessBoard.board[toLine][toColumn].getColor().equals(this.color) && chessBoard.board[toLine][toColumn] != this;
+                        return !chessBoard.board[toLine][toColumn].getColor().equals(this.color)
+                                && chessBoard.board[toLine][toColumn] != this;
                 } else return true;
             } else return false;
         } else return true;
